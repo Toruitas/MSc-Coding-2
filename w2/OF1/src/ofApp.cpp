20588ce0e46d8a263f705cc09799fc521f8b7844
Stuart Leitch <Toruitas@gmail.com>
2020-03-30 18:22:43 +0100

    Added Wk2's code

---
+++ w2/OF1/src/ofApp.cpp
@@ -0,0 +1,117 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    // var numerator = 0;
+    // var denominator = 1;
+    max_num = 20;  // max for numerator
+    max_denom = 40;  // max for denominator
+    segments = 5000;
+    spacing = 3.14159 * 2 / segments;
+    radius = 300;
+    numerator = 0;
+    denominator = 1;
+    increment_denom = true;
+    increment_num = true;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofTranslate(512,384);
+    ofSetColor(255,0,0);
+
+            
+    for(int i=0; i<=segments; i++){
+        xN = cos(spacing*i*numerator)*cos(spacing*i*denominator)*radius;
+        yN = cos(spacing*i*numerator)* sin(spacing*i*denominator)*radius;
+        
+        ofDrawLine(x,y,xN,yN);
+        x = xN;
+        y = yN;
+    }
+
+    if(numerator >= max_num){
+        increment_num = false;
+    }
+    if(numerator <= 0){
+        increment_num = true;
+    }
+    if (denominator >= max_denom){
+        increment_denom = false;
+    }
+    if (denominator <= 1){
+        increment_denom = true;
+    }
+    if(increment_num){
+        numerator +=1;
+    }else{
+        numerator -=1;
+    }
+    if(increment_denom){
+        denominator +=1;
+    }else{
+        denominator -=1;
+    }
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ w2/OF1/src/ofApp.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		int max_num, max_denom;  // max for numerator and divisor
+		int segments;
+		float spacing;
+		int radius;
+		int numerator;
+		int denominator;
+		float x,y,xN, yN;
+		bool increment_num, increment_denom;
+		
+};
